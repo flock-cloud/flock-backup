7e235bc07f9c7c75d4b06f2742179d724e302786
Guido García <guido@tid.es>
2014-12-24 17:45:58 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('backup server');
+}).listen(process.env.PORT0); // dynamic binding
+
+console.log('flock-backup at http://0.0.0.0:%d/', process.env.PORT0);

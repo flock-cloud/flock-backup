fd81b1afda3eec226cd07e9385797ce4585f9a08
Guido García <guido@tid.es>
2014-12-29 20:28:15 +0100

    dump headers

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('backup server');
+    res.end(JSON.stringify(req.headers));
 }).listen(process.env.PORT0); // dynamic binding
 
 console.log('flock-backup at http://0.0.0.0:%d/', process.env.PORT0);
